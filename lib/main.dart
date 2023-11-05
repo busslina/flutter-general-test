2c3ac1903308928b2dcdd266a9c90be6b91dc81d
Vicente <busslina@gmail.com>
2023-11-05 23:21:26 +0100

    snapshot

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,52 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
+class App extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        scrollBehavior: const MaterialScrollBehavior().copyWith(
+          dragDevices: {
+            PointerDeviceKind.mouse,
+            PointerDeviceKind.touch,
+            PointerDeviceKind.stylus,
+          },
         ),
-      ),
-    );
-  }
+        home: Column(
+          children: [
+            // Bar
+            Container(
+              height: 100,
+              width: double.infinity,
+              color: Colors.red,
+              child: ListView.builder(
+                itemCount: 25,
+                itemBuilder: (context, index) => _buildItem(index),
+                scrollDirection: Axis.horizontal,
+                physics: const BouncingScrollPhysics(),
+              ),
+            ),
+
+            // Body
+            Expanded(
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      );
+
+  Widget _buildItem(int index) => Container(
+        color: Colors.yellow,
+        width: 150,
+        child: Text(
+          'Item $index',
+          style: TextStyle(fontSize: 14),
+        ),
+      );
 }
