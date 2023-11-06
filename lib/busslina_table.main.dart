79391e803aecc7dcb465a0f16c07dfb9d1a8dced
busslina <busslina@gmail.com>
2023-11-06 03:00:17 +0100

    snapshot

--- lib/busslina_table.main.dart
+++ lib/busslina_table.main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:general_test/busslina_table/page.dart';
 
 void main() {
   runApp(App());
 }
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) => MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -16,37 +19,6 @@ class App extends StatelessWidget {
             PointerDeviceKind.stylus,
           },
         ),
-        home: Column(
-          children: [
-            // Bar
-            Container(
-              height: 100,
-              width: double.infinity,
-              color: Colors.red,
-              child: ListView.builder(
-                itemCount: 25,
-                itemBuilder: (context, index) => _buildItem(index),
-                scrollDirection: Axis.horizontal,
-                physics: const BouncingScrollPhysics(),
-              ),
-            ),
-
-            // Body
-            Expanded(
-              child: Container(
-                color: Colors.blue,
-              ),
-            ),
-          ],
-        ),
-      );
-
-  Widget _buildItem(int index) => Container(
-        color: Colors.yellow,
-        width: 150,
-        child: Text(
-          'Item $index',
-          style: TextStyle(fontSize: 14),
-        ),
+        home: const PageTable(),
       );
 }
---
+++ lib/busslina_table/page.dart
@@ -0,0 +1,19 @@
+import 'package:busslina_flutter_lightweight_lib/busslina_flutter_lightweight_lib.dart'
+    as fllib;
+import 'package:flutter/material.dart';
+
+class PageTable extends StatelessWidget {
+  const PageTable({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => fllib.BusslinaTable(
+        columnLength: 4,
+        rowLength: 100,
+        headerCellBuilder: (columnIndex) =>
+            const fllib.Label('Hey').bgColor(Colors.red).centered(),
+        cellBuilder: (columnIndex, rowIndex) =>
+            fllib.Label('Hey $columnIndex $rowIndex')
+                .bgColor(Colors.blue)
+                .centered(),
+      );
+}
