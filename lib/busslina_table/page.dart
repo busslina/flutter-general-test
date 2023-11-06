c9b48f9b37314a4ed3674a638755723a1fec3faf
Vicente <busslina@gmail.com>
2023-11-06 18:18:57 +0100

    snapshot

--- lib/busslina_table/page.dart
+++ lib/busslina_table/page.dart
@@ -1,6 +1,6 @@
 import 'package:busslina_flutter_lightweight_lib/busslina_flutter_lightweight_lib.dart'
     as fllib;
-    
+
 import 'package:flutter/material.dart';
 
 class PageTable extends StatelessWidget {
@@ -10,11 +10,14 @@ class PageTable extends StatelessWidget {
   Widget build(BuildContext context) => fllib.BusslinaTable(
         columnLength: 4,
         rowLength: 100,
-        headerCellBuilder: (columnIndex) =>
-            const fllib.Label('Hey').bgColor(Colors.red).centered(),
-        cellBuilder: (columnIndex, rowIndex) =>
-            fllib.Label('Hey $columnIndex $rowIndex')
-                .bgColor(Colors.blue)
-                .centered(),
+        headerCellBuilder: (columnIndex) => const fllib.Label(
+          'Hey',
+          fontSize: 22,
+        ).centered(),
+        cellBuilder: (columnIndex, rowIndex) => fllib.Label(
+          'Hey $columnIndex $rowIndex',
+          fontSize: 18,
+          color: const Color.fromARGB(255, 76, 71, 51),
+        ).centered(),
       );
 }
