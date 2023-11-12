3ee572040c67c961d74197fdd78008aeb22528af
Vicente <busslina@gmail.com>
2023-11-12 18:37:40 +0100

    snapshot

--- lib/busslina_table/page.dart
+++ lib/busslina_table/page.dart
@@ -1,5 +1,5 @@
-import 'package:busslina_flutter_lightweight_lib/busslina_flutter_lightweight_lib.dart'
-    as fllib;
+// import 'package:busslina_flutter_lightweight_lib/busslina_flutter_lightweight_lib.dart'
+//     as fllib;
 
 import 'package:flutter/material.dart';
 
@@ -7,18 +7,20 @@ class PageTable extends StatelessWidget {
   const PageTable({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => fllib.BusslinaTable(
-        providerKey: 'myTable',
-        columnLength: 4,
-        rowLength: 100,
-        headerCellBuilder: (columnIndex) => const fllib.Label(
-          'Hey',
-          fontSize: 22,
-        ).centered(),
-        cellBuilder: (columnIndex, rowIndex) => fllib.Label(
-          'Hey $columnIndex $rowIndex',
-          fontSize: 18,
-          color: const Color.fromARGB(255, 76, 71, 51),
-        ).centered(),
-      );
+  Widget build(BuildContext context) => throw ('Not implemented');
+  // @override
+  // Widget build(BuildContext context) => fllib.BusslinaTable(
+  //       providerKey: 'myTable',
+  //       columnLength: 4,
+  //       rowLength: 100,
+  //       headerCellBuilder: (columnIndex) => const fllib.Label(
+  //         'Hey',
+  //         fontSize: 22,
+  //       ).centered(),
+  //       cellBuilder: (columnIndex, rowIndex) => fllib.Label(
+  //         'Hey $columnIndex $rowIndex',
+  //         fontSize: 18,
+  //         color: const Color.fromARGB(255, 76, 71, 51),
+  //       ).centered(),
+  //     );
 }
